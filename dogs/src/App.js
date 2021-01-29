55b24de5fce7725c50b4ad87d1cb25bdc9ad1743
Larissa Novaes <larissanovaes00@hotmail.com>
2021-01-29 08:23:05 -0300

    adicionando hook customizado para formulários

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
-const App = () => {
-  return <div>App React</div>;
-};
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
