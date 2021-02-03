f5181f628863a0020ffeb405c94252a39b5d9308
Larissa Novaes <larissanovaes00@hotmail.com>
2021-02-03 20:28:31 -0300

    adicionando scroll infinito

--- dogs/src/Components/Feed/FeedPhotos.js
+++ dogs/src/Components/Feed/FeedPhotos.js
@@ -1,21 +1,24 @@
-import React from "react";
-import FeedPhotosItem from "./FeedPhotosItem";
-import useFetch from "../../Hooks/useFetch";
-import { PHOTOS_GET } from "../../Api";
-import Error from "../Helper/Error";
-import Loading from "../Helper/Loading";
-import styles from "./FeedPhotos.module.css";
+import React from 'react';
+import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
+      console.log('Request:', json);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
--- dogs/src/Components/User/User.js
+++ dogs/src/Components/User/User.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Feed from "../Feed/Feed";
-import UserHeader from "./UserHeader";
-import UserPhotoPost from "./UserPhotoPost";
-import UserStats from "./UserStats";
+import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
